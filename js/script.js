35ef4db8a74b8e87e8ecf8ef69cc0663115e62b0
= <=>
2025-09-15 11:25:24 -0300

    mudar o ano

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
-   document.addEventListener('DOMContentLoaded', function() {
-            const swiper = new Swiper('.swiper', {
-                // Configurações do Swiper
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        // Configurações do Swiper
+        slidesPerView: 1,
+        centeredSlides: false,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+            breakpoints: {
+            320: {
                 slidesPerView: 1,
-                centeredSlides: false,
-                spaceBetween: 20,
-                loop: true,
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                },
-                breakpoints: {
-                    320: {
-                        slidesPerView: 1,
-                    },
-                    // quando a largura da janela é >= 640px
-                    640: {
-                        slidesPerView: 2,
-                    },
-                    // quando a largura da janela é >= 1024px
-                    1024: {
-                        slidesPerView: 3,
-                    },
-                    1400:{
-                        slidesPerView: 3
-                    }
-                }
-            });
-        });
+            },
+                // quando a largura da janela é >= 640px
+            640: {
+                slidesPerView: 2,
+            },
+                // quando a largura da janela é >= 1024px
+            1024: {
+                slidesPerView: 3,
+            },
+            1400:{
+                slidesPerView: 3
+            }
+        }
+    });
+});
+
+const btn = document.getElementById('logoff')
+
+btn.addEventListener('click', () => {
+    alert('Faça Login para ter acesso')
+})
